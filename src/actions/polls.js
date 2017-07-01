535f57b0ca2a5fd254378428e850fc76a17abd17
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-01 09:07:40 +0200

    add polls actions

---
+++ src/actions/polls.js
@@ -0,0 +1,35 @@
+export const ADD_POLL = 'ADD_POLL';
+export function addPoll(pollId, ownerId, title, options) {
+  return {
+    type: ADD_POLL,
+    pollId,
+    ownerId,
+    title,
+    options
+  };
+}
+
+export const REMOVE_POLL = 'REMOVE_POLL';
+export function removePoll(pollId) {
+  return {
+    type: REMOVE_POLL,
+    pollId
+  };
+}
+
+export const VOTE = 'VOTE';
+export function vote(pollId, optionId) {
+  return {
+    type: VOTE,
+    pollId,
+    optionId
+  };
+}
+
+export const SET_VISIBLE_POLLS = 'SET_VISIBLE_POLLS';
+export function setVisiblePolls(pollIds) {
+  return {
+    type: SET_VISIBLE_POLLS,
+    pollIds
+  };
+}
