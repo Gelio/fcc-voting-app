1d0f8c527bd3822bff2077472a8c398541fc8a9b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-01 10:50:42 +0200

    format index using Prettier

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ const store = createStore(
   }),
   compose(
     applyMiddleware(middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
