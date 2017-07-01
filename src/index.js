4cf8a57783ab7bc655af6c60d45e5e5e09c1ce3b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-01 09:09:06 +0200

    use bootstrap, router and redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,31 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Router
+import { BrowserRouter } from 'react-router-dom';
+import { Router, Route,  } from 'react-router';
+import { routerReducer  } from 'react-router-redux';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(
+  combineReducers({
+  ...reducers
+}));
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
