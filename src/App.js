c94d5fecbe0a82791b9ae060185aa6557a8d6ccb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-02 19:26:35 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { Route, Switch } from 'react-router';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const footerStyle = {
+  marginTop: '40px',
+  fontSize: '0.8rem'
+};
+
+const Footer = () => {
+  return (
+    <footer className="container text-center" style={footerStyle}>
+      Created by <a href="mailto:grzegorz.rozdzialik@gmail.com">Grzegorz Rozdzialik</a> @ 2017
+    </footer>
+  );
+};
+
+export default Footer;
