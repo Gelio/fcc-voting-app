5de8e2afd86d237aecf2dfd23e32e37108ae63a0
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-02 19:20:27 +0200

    add collapsable navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 const AppNavLink = ({ exact, to, children }) => {
@@ -14,33 +14,63 @@ const AppNavLink = ({ exact, to, children }) => {
   );
 };
 
-const Navigation = ({ authenticated, username }) => {
-  return (
-    <nav className="navbar navbar-light bg-faded">
-      <h1 className="navbar-brand mb-0">
-        <Link to="/">Voting App</Link>
-      </h1>
-      <div className="navbar-collapse">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <AppNavLink exact to="/">
-              Home
-            </AppNavLink>
-          </li>
-          <li className="nav-item">
-            <AppNavLink to="/polls">Polls</AppNavLink>
-          </li>
-          {authenticated &&
+class Navigation extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleCollapse() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    const { authenticated, username } = this.props;
+
+    return (
+      <nav className="navbar navbar-toggleable navbar-light bg-faded">
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          onClick={this.toggleCollapse.bind(this)}
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <h1 className="navbar-brand mb-0">
+          <Link to="/">Voting App</Link>
+        </h1>
+
+        <div className={'navbar-collapse' + (this.state.collapsed ? ' collapse' : '')}>
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <AppNavLink to="/profile">Profile</AppNavLink>
-            </li>}
-          {authenticated
-            ? <li className="nav-item">Logout</li>
-            : <li className="nav-item">Sign in</li>}
-        </ul>
-      </div>
-    </nav>
-  );
-};
+              <AppNavLink exact to="/">
+                Home
+              </AppNavLink>
+            </li>
+            <li className="nav-item">
+              <AppNavLink to="/polls">Polls</AppNavLink>
+            </li>
+            {authenticated &&
+              <li className="nav-item">
+                <AppNavLink to="/profile">Profile</AppNavLink>
+              </li>}
+            {authenticated
+              ? <li className="nav-item">Logout</li>
+              : <li className="nav-item">Sign in</li>}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navigation;
