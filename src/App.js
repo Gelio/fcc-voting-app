40804b80a028f84d6d70a390f98ef008b651e411
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-02 20:52:13 +0200

    remove App.css, add polls route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './App.css';
 
 import { Route, Switch } from 'react-router';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import Polls from './containers/Polls';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
         <Navigation authenticated={this.props.auth.get('authenticated')} />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/polls" component={Polls} />
         </Switch>
         <Footer />
       </div>
@@ -25,7 +26,7 @@ function mapStateToProps(state) {
   return {
     auth: state.auth,
     router: state.router
-  }
+  };
 }
 
 export default connect(mapStateToProps)(App);
