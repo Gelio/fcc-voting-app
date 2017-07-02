45027f130871993bf9b93a9c53e7a59aa2463609
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-02 20:53:55 +0200

    refactor Home, format file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Home = props => {
+const Home = () => {
   return (
     <div className="container mt-2">
       <h1>Voting App</h1>
       <p>
         This web app is an assignment from the{' '}
-        <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
-        {' '}online course.
+        <a href="https://www.freecodecamp.org/">freeCodeCamp</a> online course.
       </p>
 
       <p>
-        It allows everyone to vote in polls created by authenticated users. Users can sign in using
-        Google or an email with a password.
+        It allows everyone to vote in polls created by authenticated users.
+        Users can sign in using Google or an email with a password.
       </p>
     </div>
   );
